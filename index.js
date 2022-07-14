718d7fc5fa5f7645c822480e2d6ba7ca9f58aaf1
Fogel Egor <fogel_92@bk.ru>
2022-07-14 12:40:45 +0700

    refactor: imports

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ import {
   loginValidation,
   postCreateValidation,
 } from './validations/validations.js';
-import checkAuth from './utils/checkAuth.js';
 
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
-import handleValidationErrors from './utils/handleValidationErrors.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js';
 
 mongoose
   .connect(
