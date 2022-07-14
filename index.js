a4eb4507108c9c1eb03d2d51cf087746bd0538fa
Fogel Egor <fogel_92@bk.ru>
2022-07-14 20:21:25 +0700

    feat: getLastTags

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   });
 });
 
+app.get('/tags', PostController.getLastTags);
+app.get('/posts/tags', PostController.getLastTags);
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
