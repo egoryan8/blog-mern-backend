15ccd5a3f7c5286fbd3d25ce3f1ac54717aac992
Fogel Egor <fogel_92@bk.ru>
2022-08-01 03:39:56 +0700

    fix: avatar

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -122,7 +122,7 @@ export const update = async (req, res) => {
         text: req.body.text,
         imageUrl: req.body.imageUrl,
         user: req.userId,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
       },
     );
 
--- models/Post.js
+++ models/Post.js
@@ -24,7 +24,7 @@ const PostSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
-    ImageUrl: String,
+    imageUrl: String,
   },
   {
     timestamps: true,
