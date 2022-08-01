282fc8d8f3af9f599e17cb69652ed2f0fbe62d44
Fogel Egor <fogel_92@bk.ru>
2022-08-01 23:40:42 +0700

    feat: rever commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,100 +1,104 @@
-import bcrypt from "bcrypt";
-import UserModel from "../models/user.js";
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import UserModel from '../models/User.js';
 
 export const register = async (req, res) => {
-    try {
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        });
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-        const user = await doc.save();
+    const user = await doc.save();
 
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            },
-        );
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
 
-        const { passwordHash, ...userData } = user._doc;
+    const { passwordHash, ...userData } = user._doc;
 
-        res.json({
-            ...userData,
-            token,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось зарегистрироваться',
-        });
-    }
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось зарегестрироваться',
+    });
+  }
 };
 
 export const login = async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email });
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден',
-            });
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'Неверный логин или пароль',
-            });
-        }
-
-        const token = jwt.sign({
-            _id: user._id,
-        }, 'secret123', {
-            expiresIn: '30d',
-        });
-
-        const {passwordHash, ...userData} = user._doc;
-
-        res.json({
-            ...userData,
-            token,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось авторизоваться',
-        });
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не зарегестрирован',
+      });
     }
-}
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Неверный логин или пароль',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
+    });
+  }
+};
 
 export const getMe = async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId);
+  try {
+    const user = await UserModel.findById(req.userId);
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден'
-            })
-        }
-        const {passwordHash, ...userData} = user._doc;
-
-        res.json(userData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Нет доступа',
-        });
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
     }
-}
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Нет доступа',
+    });
+  }
+};
--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,2 @@
-export { default as handleValidationErrors } from "./handleValidationErrors.js";
-export { default as checkAuth } from "./checkAuth.js";
+export { default as checkAuth } from './checkAuth.js';
+export { default as handleValidationErrors } from './handleValidationErrors.js';
